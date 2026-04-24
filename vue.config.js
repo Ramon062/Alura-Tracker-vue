3d8440081a445ba45b748e4741d05e91df22a1e3
RamonFont <ecommerce01@ctic.tec.br>
2026-04-24 00:33:59 -0300

    corrigindo configuracao para deploy vercel

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .loader('eslint-loader')
+      .tap(options => {
+        options.emitWarning = true
+        options.failOnError = false
+        return options
+      })
+  }
+}
